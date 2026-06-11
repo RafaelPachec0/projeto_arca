31072c745002004a8d8760b2a88ebbfe45abde45
RafaelPachec0 <rafaeldasilvapacheco877@gmail.com>
2026-06-11 16:32:13 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const form = document.getElementById("loginForm");
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const usuario = document.getElementById("Usuario").value;
+    const senha = document.getElementById("Senha").value;
+
+    if (usuario === "prefeitura" && senha === "pref@456") {
+        window.location.href = "./PREF/gestao_geral_pref.html";
+    } else if (usuario === "tutor" && senha === "123456") {
+        window.location.href = "./USER/home.html";
+    } else {
+        alert("Usuário ou senha inválidos.");
+    }
+});
